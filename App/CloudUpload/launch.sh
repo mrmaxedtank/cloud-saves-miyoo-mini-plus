4ea5fb391b243709e12b582c3ab01ceed83e4901
mrmaxedtank <43934408+mrmaxedtank@users.noreply.github.com>
2024-10-29 12:03:54 +0100

    Update launch.sh
    Rewritten script, to be tested.

--- App/CloudUpload/launch.sh
+++ App/CloudUpload/launch.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
-echo $0 $*
-LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPanel -t "Uploading saves" -m "Your saves are being uploaded to the cloud!" --auto
+cd $appdir/
+sysdir=/mnt/SDCARD/.tmp_update
+miyoodir=/mnt/SDCARD/miyoo
+LD_LIBRARY_PATH="$appdir/lib:/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"
+PATH="$sysdir/bin:$PATH"
+appdir=/mnt/SDCARD/App/CloudUpload
+progress=/tmp/rclone_progress
 
-cd $(dirname "$0")
+build_infoPanel() {
+    local message="$1"
+	  local title="Uploading saves"
+	
+	  infoPanel --title "$title" --message "$message" --persistent &
+	  touch /tmp/dismiss_info_panel
+    sync
+	  sleep 1
+}
 
-/mnt/SDCARD/rclone copy -P -L --no-check-certificate /mnt/SDCARD/Saves/CurrentProfile/ cloud:Onion/saves/retroarch/ 
+run_upload() {
+  build_infoPanel "Starting upload"
+  > $progress
+  /mnt/SDCARD/rclone copy -P --stats 5s --stats-one-line -L --no-check-certificate /mnt/SDCARD/Saves/CurrentProfile/ cloud:Onion/saves/retroarch/ > $progress
+  progress_message='cat $progress'
+  build_infoPanel progress_message
+}
+
+run_upload
