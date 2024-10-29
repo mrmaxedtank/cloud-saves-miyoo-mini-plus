584fa6323e79686fc1050f3588cdd7629c1793dd
mrmaxedtank <43934408+mrmaxedtank@users.noreply.github.com>
2024-10-29 14:05:29 +0100

    Update launch.sh

--- App/CloudDownload/launch.sh
+++ App/CloudDownload/launch.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
-echo $0 $*
-LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPanel -t "Downloading saves" -m "Your saves are being downloaded to your device!" --auto
+cd $appdir/
+sysdir=/mnt/SDCARD/.tmp_update
+miyoodir=/mnt/SDCARD/miyoo
+LD_LIBRARY_PATH="$appdir/lib:/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"
+PATH="$sysdir/bin:$PATH"
+appdir=/mnt/SDCARD/App/CloudDownload
+progress=/tmp/rclone_progress
 
-cd $(dirname "$0")
+build_infoPanel() {
+	local message="$1"
+	local title="Downloading saves"
+	
+	infoPanel --title "$title" --message "$message" --persistent &
+	touch /tmp/dismiss_info_panel
+  sync
+	sleep 1
+}
 
-/mnt/SDCARD/rclone copy -P -L --no-check-certificate cloud:Onion/saves/retroarch/ /mnt/SDCARD/Saves/CurrentProfile/
+run_download() {
+	build_infoPanel "Starting download"
+	> $progress
+	/mnt/SDCARD/rclone copy -P -L --no-check-certificate cloud:Onion/saves/retroarch/ /mnt/SDCARD/Saves/CurrentProfile/ > $progress
+	progress_message='cat $progress'
+	build_infoPanel progress_message
+}
+
+run_download
+build_infoPanel "Download finished"
